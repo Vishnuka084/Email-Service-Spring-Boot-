a63605a9b9996c03cc4a4f690f9a4e57f78a10ef
Vishnuka yahan <vishnukayahan84@gmail.com>
2024-03-28 10:35:47 +0530

    Updated Contnen

--- src/main/java/lk/orionux/mailwithattachment/MailwithattachmentApplication.java
+++ src/main/java/lk/orionux/mailwithattachment/MailwithattachmentApplication.java
@@ -4,6 +4,8 @@ import jakarta.mail.MessagingException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
 public class MailwithattachmentApplication {
@@ -15,6 +17,7 @@ public class MailwithattachmentApplication {
 		SpringApplication.run(MailwithattachmentApplication.class, args);
 	}
 
+	@EventListener(ApplicationReadyEvent.class)
 	public void triggerMail() throws MessagingException {
 		mailService.mailWithAttachment("vishnukayahan708@gmail.com",
 				"This is body",
