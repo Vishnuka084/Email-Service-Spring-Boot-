1f2cd1e24982efdb772bd968907655ac32f1281e
Vishnuka yahan <vishnukayahan84@gmail.com>
2024-03-28 10:30:57 +0530

    Updated Contnetn

--- src/main/java/lk/orionux/mailwithattachment/MailService.java
+++ src/main/java/lk/orionux/mailwithattachment/MailService.java
@@ -4,10 +4,13 @@ import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.mail.MailProperties;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMailMessage;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+
+import java.io.File;
+import java.nio.file.FileSystem;
 
 /**
  * Created by Vishnuka Yahan
@@ -20,13 +23,26 @@ import org.springframework.stereotype.Service;
 @Service
 public class MailService {
 
+    @Autowired
     private JavaMailSender mailSender;
 
+    @Autowired
     private  MailProperties mailProperties;
 
     public void mailWithAttachment(String to, String body, String subject, String filePath )throws MessagingException{
         MimeMessage mimeMessage = mailSender.createMimeMessage();
-        MimeMessageHelper help = new MimeMessageHelper(mimeMessage, true);
+        MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true);
 
+        helper.setSubject(subject);
+        helper.setFrom(mailProperties.getUsername());
+        helper.setText(body);
+        helper.setTo(to);
+
+        FileSystemResource file = new FileSystemResource(new File(filePath));
+        helper.addAttachment(file.getFilename(), file);
+
+        mailSender.send(mimeMessage);
+
+        System.out.println("Mail was sent successfully");
     }
 }
--- src/main/java/lk/orionux/mailwithattachment/MailwithattachmentApplication.java
+++ src/main/java/lk/orionux/mailwithattachment/MailwithattachmentApplication.java
@@ -1,13 +1,25 @@
 package lk.orionux.mailwithattachment;
 
+import jakarta.mail.MessagingException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MailwithattachmentApplication {
 
+	@Autowired
+	private MailService mailService;
+
 	public static void main(String[] args) {
 		SpringApplication.run(MailwithattachmentApplication.class, args);
+	}
+
+	public void triggerMail() throws MessagingException {
+		mailService.mailWithAttachment("vishnukayahan708@gmail.com",
+				"This is body",
+				"Demo mail with attachement",
+				"C:\\Users\\ASUS\\Documents\\Spend_Analysis\\March_2024.txt");
 	}
 
 }
